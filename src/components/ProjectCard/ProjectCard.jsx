d399c3da68e26606b336ef800f2f2be3584292d2
Hung <hnguyen97@myseneca.ca>
2022-05-19 17:05:06 -0600

    added more project

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -15,7 +15,7 @@ function ProjectCard() {
 					</div>
 					<div className="project-card__language__container">
 						<div className="project-card__language primary">React.js</div>
-						<div className="project-card__language primary">Node.js</div>
+						<div className="project-card__language primary">Context API</div>
 						<div className="project-card__language primary">Axios</div>
 						<div className="project-card__language primary">MongoDB</div>
 						<div className="project-card__language primary">Node.js</div>
@@ -78,6 +78,33 @@ function ProjectCard() {
 							<i className=" introduction__social-media__icon fa-solid fa-globe"></i>
 						</a>
 						<a href="https://github.com/nguyenhung15913/Spotify-project">
+							<i className="introduction__social-media__icon fa-brands fa-github"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="project-card__container">
+				<img src="/comfortplaces.png" alt="" />
+				<div className="project-card__description">
+					<h1 className="project-card__name">Comfort Places</h1>
+					<div className="blog-card__item_description">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quis
+						adipisci accusamus iusto magni non incidunt ipsum! Adipisci veniam
+						ullam distinctio repellendus eius aut quae natus modi quam,
+						molestias neque!
+					</div>
+					<div className="project-card__language__container">
+						<div className="project-card__language primary">Handlebar</div>
+						<div className="project-card__language primary">Node.js</div>
+						<div className="project-card__language primary">Mongoose</div>
+						<div className="project-card__language primary">MongoDB</div>
+						<div className="project-card__language primary">Mail API</div>
+					</div>
+					<div className="project-card__link">
+						<a href="https://comfortplaces.herokuapp.com/">
+							<i className=" introduction__social-media__icon fa-solid fa-globe"></i>
+						</a>
+						<a href="https://github.com/nguyenhung15913/AirBNB-homestay-Project">
 							<i className="introduction__social-media__icon fa-brands fa-github"></i>
 						</a>
 					</div>
